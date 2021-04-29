8c6124f79f4c4eec9b3207ba8b1e21fd227d6fdd
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2021-04-29 10:25:38 +0530

    Initialize frontend design with Stimulus targets

--- app/javascript/controllers/shorturl_controller.js
+++ app/javascript/controllers/shorturl_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["output"];
+  static targets = ["heading", "enterLink"];
 
   connect() {
-    this.outputTarget.textContent = "Make Your Links Smaller!";
+    this.headingTarget.textContent = "Make Your Links Smaller!";
+    this.headingTarget.className = "text-primary text-center pt-5";
+    this.enterLinkTarget.textContent = "Enter your link below";
+    this.enterLinkTarget.className = "text-success text-center pt-5";
   }
 }
