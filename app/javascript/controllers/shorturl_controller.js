91970cd07eab0b06362806b9beb144c73e1c9a4f
ABHIGYAN MAHANTA <abhigyanmahanta001@gmail.com>
2021-04-29 13:36:41 +0530

    Create input form and button  with Stimulus targets

--- app/javascript/controllers/shorturl_controller.js
+++ app/javascript/controllers/shorturl_controller.js
@@ -1,12 +1,14 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["heading", "enterLink"];
+  static targets = ["heading", "enterLink", "input", "button"];
 
   connect() {
     this.headingTarget.textContent = "Make Your Links Smaller!";
-    this.headingTarget.className = "text-primary text-center pt-5";
+    this.headingTarget.className = "text-primary pt-5";
     this.enterLinkTarget.textContent = "Enter your link below";
-    this.enterLinkTarget.className = "text-success text-center pt-5";
+    this.enterLinkTarget.className = "text-success pt-5";
+    this.inputTarget.className = "p-2 w-50 mt-5";
+    this.buttonTarget.className = "btn btn-dark mt-4";
   }
 }
